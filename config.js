aeaf5317aea97b6769f9603c28b623033ac7d85f
Swetha <61079681+SwethaSarah@users.noreply.github.com>
2020-09-05 21:50:57 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MONGODB_URL : process.env.MONGODB_URL,
-    USER : process.env.USER,
-    PASSWORD : process.env.PASSWORD,
-    MAILID : process.env.MAILID,
-    MAILPASSWORD : process.env.MAILPASSWORD
-}
