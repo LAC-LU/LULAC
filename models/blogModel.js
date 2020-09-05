6bda0a5f7819e5e82e5e7100a3a0ddd422be2ffc
Swetha <61079681+SwethaSarah@users.noreply.github.com>
2020-09-05 18:55:07 +0530

    Update blogModel.js

--- models/blogModel.js
+++ models/blogModel.js
@@ -3,10 +3,17 @@ const mongoose = require('mongoose');
 const blogSchema = new mongoose.Schema({
     title: {type: String, required: true},
     date: {type: Date, required: true},
+    caption: {type: String, required: true},
     description: {type: String,required: true},
     img:{data: Buffer,
         contentType: String
-    }
+    },
+    authorName:{type: String ,default:""},
+    authorImg:{
+        data: Buffer,
+        contentType: String
+    },
+    authorDetail:{type: String,default:""}
 });
 
 const blogModel = new mongoose.model("Blog" , blogSchema);
