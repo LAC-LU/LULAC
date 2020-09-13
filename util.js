544b28b29636d8366ab29a924e6add94e48409fc
Swetha <61079681+SwethaSarah@users.noreply.github.com>
2020-09-13 15:44:51 +0530

    Update util.js

--- util.js
+++ util.js
@@ -2,10 +2,10 @@ const session = require('express-session');
 
 const isAuth = (req,res,next) => {
     if(req.session.user){
-        return res.status(401).send({msg: "User is not supplied"});
+        next();
     }
     else{
-        next();
+        return res.status(401).redirect("/");
     }
 };
 
